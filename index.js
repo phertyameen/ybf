7fe20a9c6bfb9b608020fb91429bab0d1e8e7229
phertyameen <aminubabafatima8@gmail.com>
2023-12-01 21:58:04 +0100

    layout restructure

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarToggler = document.querySelector('.navbarToggleIcon');
+    console.log(navbarToggler)
+    const navbarNav = document.querySelector('.navbarNav');
+    console.log(navbarNav)
+
+    function hideLists() {
+        navbarNav.style.animation = 'fadeOut 700ms ease-out';
+        setTimeout(() => {
+            navbarNav.style.display = 'none';
+        }, 500);
+    }
+    
+    function showLists() {
+        navbarNav.style.animation = 'zoomIn 700ms ease-in';
+        setTimeout(() => {
+            navbarNav.style.display = 'flex';
+        }, 500);
+    }
+    function handleNavToggle() {
+        if (navbarNav.style.display === 'flex') {
+            hideLists()
+        } else {
+            showLists()
+        }
+    }
+    navbarToggler.addEventListener('click', handleNavToggle);
+});
