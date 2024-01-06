f55c53964d97b4f48d267063f9a3b60dd93704bd
phertyameen <aminubabafatima8@gmail.com>
2024-01-06 16:46:46 +0100

    nav logo changed, added footer email validation

--- index.js
+++ index.js
@@ -27,23 +27,53 @@ document.addEventListener('DOMContentLoaded', function () {
     navbarToggler.addEventListener('click', handleNavToggle);
 });
 
-const founderdiv = document.querySelector('.founderdiv')
-const coFounderdiv = document.querySelector('.coFounderdiv')
 
-founderdiv.addEventListener('mouseover', e => {
-    coFounderdiv.style.display ='none'
-    founderdiv.style.color ='#f6fcfe'
+    document.addEventListener('DOMContentLoaded', function () {
+        let newsletterForm = document.querySelector('#newsLetterForm');
+        let newsletterInput = document.querySelector('#newsletterInput');
+        let popUpParent = document.querySelector('#popUpParent');
+        let exitMark = document.querySelector('#exitMark');
 
-    founderdiv.addEventListener('mouseout', e => {
-        coFounderdiv.style.display = ''
+        console.log(newsletterForm)
+        console.log(newsletterInput)
+        console.log(popUpParent)
+        console.log(exitMark)
+
+        // Form submission handling
+        newsletterForm.addEventListener('submit', (e) => {
+
+            e.preventDefault();
+
+            // Perform validation
+            let emailInputValue = newsletterInput.value;
+            if (isValidEmail(emailInputValue)) 
+                {
+                    showPopUp()
+                    emailInputValue = ''
+                }
+            else {alert('Input a valid Email Address')}
+        });
+
+        // Close pop-up when exitMark is clicked
+        exitMark.addEventListener('click', e => {exitPopUp()});
+
+        // Email validation
+        function isValidEmail(email) {
+            let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        }
+
+        function exitPopUp() {
+            popUpParent.style.animation = 'fadeOut 700ms ease-out';
+            setTimeout(() => {
+                popUpParent.style.display = 'none';
+            }, 500);
+        }
+        
+        function showPopUp() {
+            popUpParent.style.animation = 'zoomIn 700ms ease-in';
+            setTimeout(() => {
+                popUpParent.style.display = 'flex';
+            }, 500);
+        }
     });
-})
-coFounderdiv.addEventListener('mouseover', e => {
-    founderdiv.style.display ='none'
-    coFounderdiv.style.color = '#f6fcfe'
-
-    // Handle mouseout for coFounderdiv
-coFounderdiv.addEventListener('mouseout', e => {
-    founderdiv.style.display = ''
-});
-})
